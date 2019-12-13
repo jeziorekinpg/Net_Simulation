c3737d814f842b67f0d746f45000757c2f556e82
Imperator <jeziorek.inpg@gmail.com>
2019-12-13 23:38:04 +0100

    Implementacja klasy Package

--- package.hpp
+++ package.hpp
@@ -1,10 +1,16 @@
 #ifndef NET_SIMULATION_PACKAGE_HPP
 #define NET_SIMULATION_PACKAGE_HPP
 
+#include <_bsd_types.h>
+
 class Package{
 public:
+    Package(u_int id) : id_(id) {}
+    Vehicle(const Vehicle&&) = default;
+    Package& operator=(const Package&&);
+    typename ElementID get_id() const {return _id}
 
 private:
-    typename ElementID get_id() const {return }
+    u_int id_;
 };
 #endif
--- storage_types.hpp
+++ storage_types.hpp
@@ -1,8 +1,8 @@
-//
-// Created by Imperator on 13.12.2019.
-//
-
 #ifndef NET_SIMULATION_STORAGE_TYPES_HPP
 #define NET_SIMULATION_STORAGE_TYPES_HPP
 
-#endif //NET_SIMULATION_STORAGE_TYPES_HPP
+enum class PackageQueueType {
+    FIFO,
+    LIFO
+};
+#endif
