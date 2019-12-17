417fa10540ca4c7a30c1d5c3df98073b224c2f5a
Bartłomiej <bartek.g700@gmail.com>
2019-12-17 15:11:38 +0100

    Dokończony produkt

--- package.cpp
+++ package.cpp
@@ -4,17 +4,20 @@
 std::set<ElementID> Package::freed_IDs;
 std::set<ElementID> Package::assigned_IDs{0};
 
-// trzeba dodac opcję, żeby kostruktor i destruktor pracował z tymi listami
+
 Package::Package() {
     if (freed_IDs.empty()) {
         id_ = *assigned_IDs.cend() + 1;
+        assigned_IDs.insert(id_);
     }else{
         id_ = *freed_IDs.cbegin();
+        assigned_IDs.insert(id_);
         freed_IDs.erase(freed_IDs.cbegin());
     }
 
 }
 
 Package::~Package() {
+    assigned_IDs.erase(assigned_IDs.find(id_));
     freed_IDs.insert(id_);
 }
