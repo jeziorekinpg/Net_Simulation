8016616ec72c28f74b9de8f4579eb1ec8a02ccc8
Bartłomiej <bartek.g700@gmail.com>
2019-12-18 08:02:17 +0100

    Opornie to idzie

--- storage_types.cpp
+++ storage_types.cpp
@@ -2,3 +2,27 @@
 // Created by Imperator on 13.12.2019.
 //
 
+#include "storage_types.hpp"
+
+const void PackageQueue::push(Package&& package) {
+    list_.emplace_back(std::move(package));
+}
+
+Package&& PackageQueue::pop() {
+
+}
+
+PackageQueue::PackageQueue(const PackageQueueType type) {
+    switch (type) {
+        case PackageQueueType::FIFO:
+                type_ = type;
+                
+            break;
+
+        case PackageQueueType::LIFO:
+                type_ = type;
+
+            break;
+    }
+
+}
--- storage_types.hpp
+++ storage_types.hpp
@@ -5,36 +5,41 @@
 #include <list>
 #include "package.hpp"
 
-
-class IPackageStockpile {
-public:
-    virtual const void push(Package&&) = 0;
-    virtual const bool empty() = 0;
-    virtual const size_type size() = 0;
-    std::deque<ElementID> begin() {return .begin();}
-    std::deque<ElementID> begin() const {return .cbegin();}
-    std::deque<ElementID> end() {return .end();}
-    std::deque<ElementID> end() const {return .cend();}
-};
-
 enum class PackageQueueType{
     FIFO,
     LIFO
 };
 
+class IPackageStockpile {
+public:
+    using contener_type = std::list<Package>;
+    using own_iterator = contener_type::const_iterator;
+    virtual const void push(Package&&) = 0;
+    virtual const bool empty() = 0;
+    virtual const size_type size() = 0;
+    own_iterator begin() {return list_.begin();}
+    own_iterator begin() const {return list_.cbegin();}
+    own_iterator end() {return list_.end();}
+    own_iterator end() const {return list_.cend();}
+
+};
+
 class IPackageQueue: public IPackageStockpile{
 public:
-    virtual Package pop() = 0;
+    virtual Package&& pop() = 0;
     virtual const PackageQueueType get_queue_type() = 0;
 };
 
 class PackageQueue: public IPackageQueue {
 public:
-    explicit PackageQueue(const PackageQueueType type): type_(type) {}
+    explicit PackageQueue(const PackageQueueType type);
+    const void push(Package&&) override;
+    Package&& pop() override;
 
 private:
     PackageQueueType type_;
     std::list<Package> list_;
+
 };
 
 #endif
