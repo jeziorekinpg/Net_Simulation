0a75f94c260e83088788bdd4e567248fb54ab5e0
Bartłomiej <bartek.g700@gmail.com>
2020-01-05 20:57:27 +0100

    dodany typ

--- include/types.hpp
+++ include/types.hpp
@@ -1,12 +1,13 @@
 #ifndef NET_SIMULATION_TYPES_HPP
 #define NET_SIMULATION_TYPES_HPP
 
-
+#include <functional>
 
 using ElementID = unsigned int;
 using size_type = unsigned int;
 using Time = unsigned int;
 using TimeOffset = unsigned int;
 using ProbabilityGenerator = double;
+using ProbabilityGenerator = std::function<double>
 
 #endif
