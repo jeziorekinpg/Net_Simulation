bd3810347623afd1effd5e8ad999af00a11aa9a7
KGolemo <58262773+KGolemo@users.noreply.github.com>
2020-01-05 17:04:54 +0100

    Update helpers.hpp

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -9,8 +9,6 @@
 std::random_device rd;
 std::mt19937 rng(rd());
 
-double probability_generator() {
-    return std::generate_canonical<double, 10>(rng);
-}
+double probability_generator() { return std::generate_canonical<double, 10>(rng); }
 
 #endif //NET_SIMULATION_HELPERS_HPP
