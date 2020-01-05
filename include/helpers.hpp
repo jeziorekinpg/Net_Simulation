0b459c7cda782b212bbd9d82c69c62e87cabffe5
KGolemo <58262773+KGolemo@users.noreply.github.com>
2020-01-05 17:03:33 +0100

    Update helpers.hpp

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -1,8 +1,16 @@
-//
-// Created by Imperator on 04.01.2020.
-//
-
 #ifndef NET_SIMULATION_HELPERS_HPP
 #define NET_SIMULATION_HELPERS_HPP
+
+#include <cstdlib>
+#include <iostream>
+#include <random>
+#include <ctime>
+
+std::random_device rd;
+std::mt19937 rng(rd());
+
+double probability_generator() {
+    return std::generate_canonical<double, 10>(rng);
+}
 
 #endif //NET_SIMULATION_HELPERS_HPP
