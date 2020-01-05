40b73d076efd28e462048fe21539a82270b8cbca
Bartłomiej <bartek.g700@gmail.com>
2020-01-05 15:48:51 +0100

    Kolejne poprawki

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -9,22 +9,25 @@
 #include "storage_types.hpp"
 #include <optional>
 
-enum class NodeType{
+enum class NodeType {
     WORKER;
     STOREHOUSE;
     RAMP;
 };
 
-class Storehouse : public IPackageStockpile, IPackageReceiver{
+class Storehouse : public IPackageStockpile, IPackageReceiver {
 public:
-    Storehouse(id: ElementID, d: std::unique_ptr<IPackageStockpile);
+    Storehouse(ElementID, std::unique_ptr<IPackageStockpile);
 };
 
-class Worker : public IPackageReceiver, PackageSender, IPackageQueue{
+class Worker : public IPackageReceiver, PackageSender, IPackageQueue {
 public:
-    void Worker(id: ElementID, pd: TimeOffset, q: std::unique_ptr<IPackageQueue>) {};
-    void do_work(t: Time) {};
+    void Worker(ElementID, TimeOffset, std::unique_ptr<IPackageQueue>) {};
+
+    void do_work(Time) {};
+
     const TimeOffset get_processing_duration() {};
+
     const Time get_package_processing_start_time() {};
 
 };
@@ -32,21 +35,26 @@ public:
 class IPackageReceiver {
 public:
 
-    virtual const void receive_package(Package&&) = 0;
-;
-};    virtual const ElementID get_id() = 0
+    virtual const void receive_package(Package&&) = 0;;
+};
 
-class Ramp : public PackageSender{
+virtual const ElementID get_id() = 0
+
+class Ramp : public PackageSender {
 public:
-    void Ramp(id: ElementID, di: TimeOffset) {};
-    void deliver_goods(t: Time) {};
+    void Ramp(ElementID, TimeOffset) {};
+
+    void deliver_goods(Time) {};
+
     const TimeOffset get_delivery_interval() {};
+
     const ElementID get_id() {};
 };
 
-class PackageSender : public ReceiverPreferences{
+class PackageSender : public ReceiverPreferences {
 public:
     void send_package() {};
+
     const std::optional<Package> get_sending_buffer() {};
 private:
     ReceiverPreferences receiver_preferences_;
@@ -55,10 +63,16 @@ protected:
 };
 
 class ReceiverPreferences {
+    using preferences_t = std::map<IPackageReceiver*, double>;
+    using const_iterator = preferences_t::const_iterator;
+
 public:
-    ReceiverPreferences(pg: ProbabilityGenerator);
-    void add_receiver(r: IPackageReceiver*) {};
-    void remove_receiver(r: IPackageReceiver*) {};
+    ReceiverPreferences(ProbabilityGenerator);
+
+    void add_receiver(IPackageReceiver*) {};
+
+    void remove_receiver(IPackageReceiver*) {};
+
     IPackageReceiver* choose_receiver() {};
 
 };
