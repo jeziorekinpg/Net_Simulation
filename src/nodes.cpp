05a9ca4931ed48f3fdd18cb3498f49197d871a2f
jeziorekinpg <49477687+jeziorekinpg@users.noreply.github.com>
2020-01-11 18:32:16 +0100

    Update nodes.cpp

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,9 +1,11 @@
 #include "nodes.hpp"
 
+#include <utility>
+
 // ReceiverPreferences
 
 ReceiverPreferences::ReceiverPreferences(ProbabilityGenerator probability_gen) {
-    probability_generator_ = probability_gen;
+    probability_generator_ = std::move(probability_gen);
 }
 
 ReceiverPreferences::ReceiverPreferences() {
@@ -16,9 +18,9 @@ void ReceiverPreferences::add_receiver(IPackageReceiver* r) {
         preferences[r] = 1;
     } else {
         for (auto& element : preferences) {
-            element.second = 1 / preferences.size() + 1;
+            element.second = 1.0 / preferences.size() + 1;
         }
-        preferences[r] = 1 / preferences.size() + 1;
+        preferences[r] = 1.0 / preferences.size() + 1;
     }
 }
 
@@ -26,7 +28,7 @@ void ReceiverPreferences::remove_receiver(IPackageReceiver* r) {
     if (preferences.find(r) != preferences.end()) {
         preferences.erase(r);
         for (auto& element : preferences) {
-            element.second = 1 / preferences.size();
+            element.second = 1.0 / preferences.size();
         }
     } else {
         throw std::invalid_argument("There is no such receiver");
@@ -37,7 +39,7 @@ IPackageReceiver* ReceiverPreferences::choose_receiver() {
     double wylosowane = probability_generator_();
     double suma = 0.0;
     IPackageReceiver* to_return = nullptr;
-//TODO sprawdzić poprawnośc tej metody
+//TODO sprawdzic poprawnosc tej metody
     for (auto& element : preferences) {
         suma += element.second;
         if (suma > wylosowane) {
@@ -64,14 +66,9 @@ void PackageSender::send_package() {
 
 }
 
-const std::optional<Package> PackageSender::get_sending_buffer() {
-    std::optional<Package> xyz = std::nullopt;
-    if(buffer_.has_value()){
-        xyz = std::move(buffer_.value());
-    }
-    return (xyz);
+std::optional<Package>& PackageSender::get_sending_buffer() const {
+    return (std::optional<Package>&) buffer_;
 
-    //return(buffer_.value_or(std::nullopt));
 }
 
 void PackageSender::push_package(Package&& to_send) {
@@ -97,16 +94,6 @@ void Ramp::deliver_goods(Time t) {
 }
 
 
-
-// Implementacja klasy Worker 
-//TODO Sprawdzić, poprawić
-/* Bufor jest zrobiony jako pole klasy jeszcze w .hpp. Tutaj sprawdzam czy bufor jest wolny i jeśli jest, to wpisuje do niego 
- * produkt z kolejki (tutaj nie wiem czy dobrze rozumiem kolejke i produkt w niej - bo po prostu przypisałem wartość 
- * z unique_ptr, a pewnie zupełnie o coś innego chodzi. Wraz z przypisaniem produktu, przypisuje czas rozpoczęcia przetwarzania 
- * (st_ = t). Następnie sprawdzam czy już skończył przetwarzać produkt. Nie skończył, bo w tym takcie dopiero przypisaliśmy 
- * produkt do bufora i czas startu. Dopiero przy drugim obiegu pierwszy if zostanie pominięty, bo bufor będzie już zajęty i 
- * być może spełnimy już warunek 2. ifa i użyjemy metody z PackageSendera, wyczyścimy bufor i czas rozpoczęcia przetwarzania.
- */
 void Worker::do_work(Time t) {
     if (!buffer.has_value()) {
         buffer = q_->pop();
@@ -126,11 +113,7 @@ Worker::Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q, Re
 
 }
 
-//Ponieważ większości użytkowników klasy Storehouse nie interesuje to, w jaki sposób dokładnie magazyn przechowuje półprodukty, 
-//w jej konstruktorze zdefiniuj odpowiedni argument domyślny dla parametru typu std::unique_ptr<IPackageStockpile> 
-//(stwórz inteligentny wskaźnik na nowy obiekt typu PackageQueue; nie ma znaczenia, czy będzie to kolejka FIFO, czy LIFO).
-//Jak stworzyć argument domyślny jako nowy obiekt typy PackageQueue???
-Storehouse::Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d = std::move(PackageQueue(PackageQueueType::FIFO))) {
+Storehouse::Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d = std::make_unique<PackageQueue>(PackageQueue(PackageQueueType::FIFO))) {
   id_ = id,
   d_ = std::move(d);
   type_ = NodeType::STOREHOUSE;
