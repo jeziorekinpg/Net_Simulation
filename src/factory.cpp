bf2b0a3201c3abdd64fbfaf52508826179d17e51
jeziorekinpg <49477687+jeziorekinpg@users.noreply.github.com>
2020-01-15 22:40:02 +0100

    Update factory.cpp
    is_consistent

--- src/factory.cpp
+++ src/factory.cpp
@@ -1,1 +1,26 @@
 #include "factory.hpp"
+#include <map>
+
+bool Factory::is_consistent() const {
+    std::map<PackageSender*, NodeColor> color;
+
+    for(const auto& element: list_ramp) {
+        color[(PackageSender *) &element] = NodeColor::UNVISITED;
+    }
+
+    for(const auto& element: list_worker) {
+        color[(PackageSender *) &element] = NodeColor::UNVISITED;
+    }
+
+    for(const auto& element: list_ramp) {
+
+        try {
+            has_reachable_storehouse(&element, color)
+            return true;
+        }
+        catch(NoReceiverException&, noReceiversException) {
+            return false;
+        }
+    }
+    return true;
+};
