52e744fb515c1811d45abea50d0114be10bcf732
jeziorekinpg <49477687+jeziorekinpg@users.noreply.github.com>
2020-01-15 22:40:45 +0100

    Update factory.hpp
    drobne zmiany factory

--- include/factory.hpp
+++ include/factory.hpp
@@ -1,56 +1,63 @@
 #ifndef NET_SIMULATION_FACTORY_HPP
 #define NET_SIMULATION_FACTORY_HPP
 
-enum class ReceiverType {
-    WORKER,
-    STOREHOUSE
-};
-
+#include "nodes.hpp"
 enum class NodeColor {
     UNVISITED,
     VISITED,
     VERIFIED
 };
 
-bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
-bool Factory::is_consistent();
+enum class ReceiverType {
+  WORKER,
+  STOREHOUSE
+};
+
+bool has_reachable_storehouse(const PackageSender*, std::map<const PackageSender*, NodeColor>&) const;
 
 class NodeCollection<Node> {
-    using container_t = typename std_container_t<Node>;
+    using container_t = typename std::vector<Node>;
     using iterator = typename container_t::iterator;
     using const_iterator = typename container_t::const_iterator;
 
     void add(Node& node);
     void remove_by_id(ElementID id_);
     NodeCollection<Node>::iterator find_by_id(ElementID id_);
-    NodeCollection<Node>::const_iterator find_by_id(ElementID id_); // const
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id_);
 };
 
 class Factory {
 public:
     void add_ramp(Ramp&&);
     void remove_ramp(ElementID id);
-
     NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id);
-    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id); //const
-    NodeCollection<Ramp>::const_iterator ramp_cbegin(); //const
-    NodeCollection<Ramp>::const_iterator ramp_cend(); //const
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id);
+    NodeCollection<Ramp>::const_iterator ramp_cbegin();
+    NodeCollection<Ramp>::const_iterator ramp_cend();
 
+    void add_worker(Worker&&);
+    void remove_worker(ElementID id)
     NodeCollection<Worker>::iterator find_worker_by_id(ElementID id);
     NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id); //const
     NodeCollection<Worker>::const_iterator worker_cbegin(); //const
     NodeCollection<Worker>::const_iterator worker_cend(); //const
 
+    void add_storehouse(Storehouse&&);
+    void remove_storehouse(ElementID id);
     NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id);
     NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id); //const
     NodeCollection<Storehouse>::const_iterator storehouse_cbegin(); //const
     NodeCollection<Storehouse>::const_iterator storehouse_cend(); //const
 
-    bool is_consistent();
+    bool is_consistent() const;
     void do_deliveries(Time);
     void do_package_passing();
     void do_work(Time);
 private:
     void remove_receiver(NodeCollection<Node>& collection, ElementID id);
+    NodeCollection<Worker> list_worker;
+    NodeCollection<Ramp> list_ramp;
+    NodeCollection<Storehouse> list_storehouse;
+
 };
 #endif //NET_SIMULATION_FACTORY_HPP
