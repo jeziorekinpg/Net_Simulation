4faeef4171ec35034697c0bc712bf21a5556bffd
jeziorekinpg <49477687+jeziorekinpg@users.noreply.github.com>
2020-01-16 03:43:26 +0100

    Update factory.cpp

--- src/factory.cpp
+++ src/factory.cpp
@@ -12,15 +12,36 @@ bool Factory::is_consistent() const {
         color[(PackageSender *) &element] = NodeColor::UNVISITED;
     }
 
-    for(const auto& element: list_ramp) {
+    //for(const auto& element: list_ramp) {
 
-        try {
-            has_reachable_storehouse(&element, color)
-            return true;
-        }
-        catch(NoReceiverException&, noReceiversException) {
-            return false;
-        }
-    }
+     //   try {
+     //       has_reachable_storehouse(&element, color)
+     //       return true;
+     //   }
+     //   catch(NoReceiverException& noReceiversException) {
+     //       return false;
+      //  }
+    //}
     return true;
-};
+}
+
+void Factory::do_deliveries(Time t) {
+  for(auto iter = list_ramp.begin(); iter <= list_ramp.end(); iter++){
+    iter->deliver_goods(t);
+  }
+}
+
+void Factory::do_work(Time t) {
+  for(auto iter = list_worker.begin(); iter <= list_worker.end(); iter++){
+    iter->do_work(t);
+  }
+}
+
+void Factory::do_package_passing() {
+  for(auto iter = list_ramp.begin(); iter <= list_ramp.end(); iter++){
+    iter->send_package();
+  }
+  for(auto iter = list_worker.begin(); iter <= list_worker.end(); iter++){
+    iter->send_package();
+  }
+}
