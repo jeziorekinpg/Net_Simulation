2ae183152723a8af31335039eb325f690d367c38
jeziorekinpg <49477687+jeziorekinpg@users.noreply.github.com>
2020-01-19 15:33:09 +0100

    Add files via upload

---
+++ src/reports.cpp
@@ -0,0 +1,19 @@
+#include "reports.hpp"
+
+#include <utility>
+
+SpecificTurnsReportNotifier::SpecificTurnsReportNotifier(std::set<Time> tset) {
+  tset_ = std::move(tset);
+}
+
+bool SpecificTurnsReportNotifier::should_generate_report(Time t) {
+  return tset_.find(t) != tset_.end();
+};
+
+IntervalReportNotifier::IntervalReportNotifier(TimeOffset toffs) {
+  toffs_ = toffs;
+}
+
+bool IntervalReportNotifier::should_generate_report(Time t) {
+  return (t == 1) || (t % toffs_ == 1);
+};
