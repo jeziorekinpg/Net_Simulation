e90f21ddcb188c389e24d486094cb642a52c96fa
Bartłomiej <bartek.g700@gmail.com>
2020-01-19 16:41:43 +0100

    jeszcze jeden commit

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -5,11 +5,14 @@
 #include "simulation.hpp"
 
 void simulate(Factory& f, TimeOffset d, std::function<void(Factory&, Time)> rf) {
+    if(!f.is_consistent()){
+        throw std::logic_error("not consistent graph");
+    }
     for(TimeOffset i = 1; i <= d; i++) {
         f.do_deliveries(i);
         f.do_package_passing();
         f.do_work(i);
-
+        if()
     }
 
 }
